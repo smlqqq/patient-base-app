ada183280f8b81ff6e3db390d35ac3862a316698
smlqqq <sined_9206@mail.ru>
2022-08-26 10:55:18 +0300

    + db

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.patientbaseapp {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.patientbaseapp to javafx.fxml;
     exports com.example.patientbaseapp;
