f373166fbca2d6688304b60df6e37dc08efd41f4
smlqqq <78303132+smlqqq@users.noreply.github.com>
2022-08-31 14:02:37 +0300

    test

---
+++ src/main/java/com/example/patientbaseapp/Log/DocAcc.java
@@ -0,0 +1,27 @@
+//package com.example.patientbaseapp.Log;
+//
+//public class DocAcc {
+//    private String ID;
+//    private String Password;
+//
+//    public DocAcc(String ID, String password) {
+//        this.ID = ID;
+//        Password = password;
+//    }
+//
+//    public String getID() {
+//        return ID;
+//    }
+//
+//    public void setID(String ID) {
+//        this.ID = ID;
+//    }
+//
+//    public String getPassword() {
+//        return Password;
+//    }
+//
+//    public void setPassword(String password) {
+//        Password = password;
+//    }
+//}
