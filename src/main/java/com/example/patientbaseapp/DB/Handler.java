e4349698ae55e2528a1a871eb46f2c0f697adb99
smlqqq <78303132+smlqqq@users.noreply.github.com>
2022-09-03 22:06:37 +0300

    update 0.02a (3.9.2022)

---
+++ src/main/java/com/example/patientbaseapp/DB/Handler.java
@@ -0,0 +1,34 @@
+package com.example.patientbaseapp.DB;
+
+import com.example.patientbaseapp.Domain.Docs;
+
+import java.sql.*;
+
+public class Handler extends Configs{
+    Connection dbConnection;
+    public Connection getDbConnection() throws ClassNotFoundException, SQLException {
+
+        String connectionString = "jdbc:postgresql://" + dbHost + ":"
+                + dbPort + "/" + dbName;
+
+
+        Class.forName("org.postgresql.Driver");
+        dbConnection = DriverManager.getConnection(connectionString,dbUser,dbPass);
+        return dbConnection;
+    }
+    public ResultSet getAccount(Docs docs){
+        ResultSet resultSet = null;
+        String select = "select * from hospital.docs where login =? AND password =?";
+
+
+        try {
+            PreparedStatement preparedStatement = getDbConnection().prepareStatement(select);
+            preparedStatement.setString(1, docs.getLogin());
+            preparedStatement.setString(2, docs.getPassword());
+            resultSet = preparedStatement.executeQuery();
+        } catch (SQLException | ClassNotFoundException e) {
+
+        }
+        return resultSet;
+    }
+}
