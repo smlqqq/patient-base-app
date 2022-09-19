8ba2ad19b04a0d4d31c3c88ce3f635722cdd69cb
smlqqq <78303132+smlqqq@users.noreply.github.com>
2022-09-19 21:42:22 +0300

    update (19.9.2022)

--- src/main/java/com/example/patientbaseapp/AfterLogin.java
+++ src/main/java/com/example/patientbaseapp/AfterLogin.java
@@ -1,7 +1,6 @@
 package com.example.patientbaseapp;
 
 import com.example.patientbaseapp.DB.Configs;
-import com.example.patientbaseapp.DB.Handler;
 import com.example.patientbaseapp.Domain.Patients;
 
 
@@ -15,9 +14,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.util.Callback;
 
 import java.io.IOException;
@@ -25,6 +24,18 @@ import java.sql.*;
 
 public class AfterLogin  extends Configs {
     private ObservableList<ObservableList> data;
+
+    @FXML
+    private TableView patientsTable;
+    @FXML
+    private TableColumn<Patients, Integer>patIDcolumn;
+    @FXML
+    private TableColumn<Patients, String>patNamecolumn;
+    @FXML
+    private TableColumn<Patients, String>patSurnamecolumn;
+    @FXML
+    private TableColumn<Patients, String>patDayOfBirthcolumn;
+
     @FXML
     private TableView getPatientTable;
     @FXML
@@ -39,7 +50,6 @@ public class AfterLogin  extends Configs {
     private TableColumn<Patients, String> patientAge;
     @FXML
     private TextArea getPatientDiagnosis;
-
     @FXML
     private Button regPatientBtn;
     @FXML
@@ -55,26 +65,15 @@ public class AfterLogin  extends Configs {
                 throw new RuntimeException(ex);
             }
         });
-
-//        regPatientBtn.setOnAction(actionEvent->{
-//            newScene("registrationPat.fxml");
-//        });
-
-
-
     }
 
     public void setPatientsData(ActionEvent e) throws IOException {
-//        regPatientBtn.setOnAction(actionEvent->{
-//            newScene("registrationPat.fxml");
-//        });
-
         //Close current
         Stage stage = (Stage) regPatientBtn.getScene().getWindow();
         // do what you have to do
         stage.close();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("registrationPat.fxml"));
-        Parent root1 = (Parent) fxmlLoader.load();
+        Parent root1 = fxmlLoader.load();
         stage = new Stage();
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.setTitle("Registration Form");
@@ -83,26 +82,50 @@ public class AfterLogin  extends Configs {
 
     }
 
-
-
-
-
-
     Connection dbConnection;
 
     public Connection getDbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        return DriverManager.getConnection("jdbc:postgresql://abul.db.elephantsql.com:5432/ckkttdhb", "ckkttdhb", "nozrUH1mHHpvvm8s9L_JPAgb1bm14w20");
 
-        String connectionString = "jdbc:postgresql://" + dbHost + ":"
-                + dbPort + "/" + dbName;
-
-
-        Class.forName("org.postgres.Driver");
-        dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
-        return dbConnection;
+//        String connectionString = "jdbc:postgresql://" + dbHost + ":"
+//                + dbPort + "/" + dbName;
+//
+//
+//        Class.forName("org.postgres.Driver");
+//        dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
+//        return dbConnection;
     }
 
 
     public void getPatient() throws SQLException, ClassNotFoundException {
+//        String selectPatients = "SELECT id, first_name, second_name, day_of_birth FROM hospital_db.patients";
+//try {
+//
+//
+//    Connection c = getDbConnection();
+//    ObservableList<Patients> patients = FXCollections.observableArrayList();
+//  //  data = FXCollections.observableArrayList();
+//    ResultSet rs = c.createStatement().executeQuery(selectPatients);
+//
+//    while (rs.next()) {
+//        Integer patID = rs.getInt("id");
+//        String patName = rs.getString("first_name");
+//        String patSurName = rs.getString("second_name");
+//        String patDOB = rs.getString("day_of_birth");
+//        patients.add(new Patients(patID, patName, patSurName, patDOB));
+//    }
+//    patientID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+//    patientName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+//    patientSurname.setCellValueFactory(new PropertyValueFactory<>("SurName"));
+//    patientAge.setCellValueFactory(new PropertyValueFactory<>("Age(DOB)"));
+//    getPatientTable.setItems(patients);
+//} catch (ClassNotFoundException e) {
+//    throw new RuntimeException(e);
+//} catch (SQLException e) {
+//    throw new RuntimeException(e);
+//}
+
         Connection c = getDbConnection();
         data = FXCollections.observableArrayList();
 
@@ -118,58 +141,35 @@ public class AfterLogin  extends Configs {
 //            throw new RuntimeException(e);
 //        }
 
-
-       try {
-
-            String selectPatients = "SELECT * FROM hospital_db.patients  where id =? AND first_name =? AND second_name =? AND day_of_birth =?";
-
+        try {
+            String selectPatients = "SELECT * FROM hospital_db.patients";
             ResultSet rs = c.createStatement().executeQuery(selectPatients);
             for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
                 //We are using non property style for making dynamic table
                 final int j = i;
                 TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
-                col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
-                    public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
-                        return new SimpleStringProperty(param.getValue().get(j).toString());
-                    }
-                });
-
-
+                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
+                System.out.println(col);
                 getPatientTable.getColumns().addAll(col);
             }
-
-
-            while(rs.next()){
+            while (rs.next()) {
                 //Iterate Row
                 ObservableList<String> row = FXCollections.observableArrayList();
-                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                     //Iterate Column
+                    System.out.println(row);
                     row.add(rs.getString(i));
+
                 }
-
                 data.add(row);
-
             }
             getPatientTable.setItems(data);
-
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-    }
-    public void newScene(String window) {
-        regPatientBtn.getScene().getWindow().hide();
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource(window));
-        try {
-            fxmlLoader.load();
-        } catch (IOException e) {  }
-
-        Parent root = fxmlLoader.getRoot();
-        Stage stage = new Stage();
-        stage.setScene(new Scene(root));
-        stage.showAndWait();
-    }
 
     }
+}
+
+
 
