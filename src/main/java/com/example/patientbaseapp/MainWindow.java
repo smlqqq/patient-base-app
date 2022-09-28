65bd8aa2f31f7043640f364a8c7657e02dddd072
smlqqq <78303132+smlqqq@users.noreply.github.com>
2022-09-29 02:37:08 +0300

    update v4 (29.9.2022)

--- src/main/java/com/example/patientbaseapp/MainWindow.java
+++ src/main/java/com/example/patientbaseapp/MainWindow.java
@@ -15,31 +15,20 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.w3c.dom.events.MouseEvent;
 
 import java.io.IOException;
 import java.sql.*;
 
 public class MainWindow extends Configs {
+
     private ObservableList<ObservableList> data;
-
-
-    @FXML
-    private TableView patientsTable;
-    @FXML
-    private TableColumn<Patients, Integer> patIDcolumn;
-    @FXML
-    private TableColumn<Patients, String> patNamecolumn;
-    @FXML
-    private TableColumn<Patients, String> patSurnamecolumn;
-    @FXML
-    private TableColumn<Patients, String> patDayOfBirthcolumn;
-
     @FXML
     private TableView getPatientTable;
     @FXML
     private Button pateintReloadDB;
     @FXML
-    private TableColumn<Patients, Integer> patientID;
+    private TableColumn<Patients, String> patientID;
     @FXML
     private TableColumn<Patients, String> patientName;
     @FXML
@@ -47,46 +36,42 @@ public class MainWindow extends Configs {
     @FXML
     private TableColumn<Patients, String> patientAge;
     @FXML
+    private TableColumn<Patients, String> patientDiagnosis;
+    @FXML
     private TextArea getPatientDiagnosis;
     @FXML
     private Button regPatientBtn;
     @FXML
     private Button backBtn;
+    @FXML
+    private TextField nameText;
+    @FXML
+    private TextField surnameText;
+    @FXML
+    private TextField dateOfBirth;
+    @FXML
+    private TextField diagnosisText;
+    @FXML
+    private Button patientAdd;
+    @FXML
+    private TextField nameText1;
+    @FXML
+    private TextField surnameText1;
+    @FXML
+    private TextField idText;
+    @FXML
+    private TextField diagnosisText1;
+    @FXML
+    private TextField dobText;
 
 
-//    ObservableList<Patients> oblist = FXCollections.observableArrayList();
-
-    public void PatientData(ActionEvent e)  {
-
-//          String selectPatients = "SELECT id, first_name, second_name, day_of_birth FROM hospital_db.patients";
-//          String selectPatients = "SELECT * FROM hospital_db.patients";
-//
-//            Connection connection = getDbConnection();
-//            ObservableList <Patients> patients = FXCollections.observableArrayList();
-//            //  data = FXCollections.observableArrayList();
-//            ResultSet rs = connection.createStatement().executeQuery(selectPatients);
-//
-//            while (rs.next()){
-//                oblist.add(new Patients(rs.getInt("id"), rs.getString("first_name"), rs.getString("second_name"), rs.getString("day_of_birth")));
-//            }
-//
-//        patientID.setCellValueFactory(new PropertyValueFactory<>("id"));
-//        patientName.setCellValueFactory(new PropertyValueFactory<>("firstName1"));
-//        patientSurname.setCellValueFactory(new PropertyValueFactory<>("lastName1"));
-//        patientAge.setCellValueFactory(new PropertyValueFactory<>("dayOfBirth1"));
-//
-//
-//
-//        getPatientTable.setItems(oblist);
+    PreparedStatement pst;
+    ResultSet rs;
+    Connection dbConnection;
+    int index = -1;
 
 
-
-
-
-
-
-
-
+    public void PatientData(ActionEvent e) {
 
         pateintReloadDB.setOnAction(actionEvent -> {
 
@@ -97,22 +82,8 @@ public class MainWindow extends Configs {
             } catch (ClassNotFoundException ex) {
                 throw new RuntimeException(ex);
             }
-            //            onEdit();
-
-
-//                onEdit();
         });
 
-//        if (getPatientTable.getSelectionModel().getSelectedItem() != null) {
-//            Patients selectedPatient = (Patients) getPatientTable.getSelectionModel().getSelectedItem();
-//            patientID.setText(String.valueOf(selectedPatient.getId()));
-//            patientName.setText(selectedPatient.getFirstName());
-//            patientSurname.setText(selectedPatient.getLastName());
-//            patientAge.setText(selectedPatient.getDayOfBirth());
-//
-//
-//
-//        }
 
     }
 
@@ -132,7 +103,6 @@ public class MainWindow extends Configs {
 
     }
 
-    Connection dbConnection;
 
     public Connection getDbConnection() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
@@ -205,41 +175,42 @@ public class MainWindow extends Configs {
     }
 
     public void getDataFromDB(ActionEvent actionEvent) {
-        String selectPatients = "SELECT id, first_name, second_name, day_of_birth FROM hospital_db.patients";
+        String selectPatients = "SELECT * FROM hospital_db.patients";
 //        String selectPatients = "SELECT * FROM hospital_db.patients";
         try {
-            Connection connection = getDbConnection();
+            dbConnection = getDbConnection();
             final ObservableList<Patients> patients = FXCollections.observableArrayList();
             //  data = FXCollections.observableArrayList();
-            ResultSet rs = connection.createStatement().executeQuery(selectPatients);
+            rs = dbConnection.createStatement().executeQuery(selectPatients);
 
             while (rs.next()) {
                 String id = rs.getString("id");
                 String first_name = rs.getString("first_name");
                 String second_name = rs.getString("second_name");
                 String day_of_birth = rs.getString("day_of_birth");
+                String diagnosis = rs.getString("diagnosis");
 
-                patients.add(new Patients(id, first_name, second_name, day_of_birth));
+                patients.add(new Patients(id, first_name, second_name, day_of_birth, diagnosis));
             }
 
-            TableColumn<Patients, String> id = new TableColumn<>("ID");
-            id.setCellValueFactory(cellData -> cellData.getValue().IDProperty());
+            patientID = new TableColumn<>("ID");
+            patientID.setCellValueFactory(cellData -> cellData.getValue().IDProperty());
 
-            TableColumn<Patients, String> name = new TableColumn<>("Name");
-            name.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+            patientName = new TableColumn<>("Name");
+            patientName.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
 
-            TableColumn<Patients, String> surname = new TableColumn<>("Last Name");
-            surname.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+            patientSurname = new TableColumn<>("Last Name");
+            patientSurname.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
 
+            patientAge = new TableColumn<>("Day Of Birth");
+            patientAge.setCellValueFactory(cellData -> cellData.getValue().dayOfBirthProperty());
 
-            TableColumn<Patients, String> age = new TableColumn<>("Day Of Birth");
-            age.setCellValueFactory(cellData -> cellData.getValue().dayOfBirthProperty());
+            patientDiagnosis = new TableColumn<>("Diagnosis");
+            patientDiagnosis.setCellValueFactory(cellData -> cellData.getValue().diagnosisProperty());
 
-
-            getPatientTable.getColumns().addAll(id, name, surname, age);
+            getPatientTable.getColumns().addAll(patientID, patientName, patientSurname, patientAge, patientDiagnosis);
             getPatientTable.setColumnResizePolicy(getPatientTable.CONSTRAINED_RESIZE_POLICY);
             getPatientTable.setItems(patients);
-
 
 
         } catch (ClassNotFoundException e) {
@@ -250,34 +221,48 @@ public class MainWindow extends Configs {
     }
 
 
-    public void Reload(ActionEvent actionEvent) {
+    public void Reload(ActionEvent actionEvent) throws SQLException {
         getPatientTable.getItems().clear();
         getPatientTable.getColumns().clear();
         getDataFromDB(actionEvent);
+        dbConnection.close();
 
     }
+
     public void Search(ActionEvent actionEvent) {
     }
 
     public void Update(ActionEvent actionEvent) {
+        try {
+            dbConnection = getDbConnection();
+            String data1 = patientID.getText();
+            String data2 = patientName.getText();
+            String data3 = patientSurname.getText();
+//            String data4 = patientDiagnosis.getText();
+
+            String setSelect = "UPDATE hospital_db.patients set first_name = " + data2 + ",second_name= " + data3;// + ",diagnosis= " + data4;
+            pst = dbConnection.prepareStatement(setSelect);
+            pst.execute();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @FXML
-    private TextField nameText1;
-    @FXML
-    private TextField surnameText1;
     public void Delete(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
-        Connection connection = getDbConnection();
-        String selectPatients = "DELETE from hospital_db.patients where id =?  ";
-        PreparedStatement pst;
+        dbConnection = getDbConnection();
+        String selectPatients = "DELETE from hospital_db.patients where id =?";
         try {
 //            getPatientTable.getItems().removeAll(getPatientTable.getSelectionModel().getSelectedItem());
 
-            pst = connection.prepareStatement(selectPatients);
+            pst = dbConnection.prepareStatement(selectPatients);
 //            pst.setString(1, nameText1.getText());
+
             pst.setString(1, nameText1.getText());
             pst.execute();
-            getDataFromDB(actionEvent);
+//            getDataFromDB(actionEvent);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -286,26 +271,17 @@ public class MainWindow extends Configs {
 
     //PATIENTS ADD
 //----------------------------------------------------------------------------------------------------------------------
-    @FXML
-    private TextField nameText;
-    @FXML
-    private TextField surnameText;
-    @FXML
-    private TextField dateOfBirth;
-    @FXML
-    private TextField diagnosisText;
-    @FXML
-    private Button patientAdd;
-    public void addPatients (String setFirstName, String setLastName, String setDayOfBirth, String setDiagnosis) {
+
+    public void addPatients(String setFirstName, String setLastName, String setDayOfBirth, String setDiagnosis) {
         String setSelect = "insert into hospital_db.patients (first_name,second_name,day_of_birth,diagnosis)VALUES (?,?,?,?)";
 
         try {
-            PreparedStatement preparedStatement = getDbConnection().prepareStatement(setSelect);
-            preparedStatement.setString(1, setFirstName);
-            preparedStatement.setString(2, setLastName);
-            preparedStatement.setString(3, setDayOfBirth);
-            preparedStatement.setString(4, setDiagnosis);
-            preparedStatement.executeUpdate();
+            pst = getDbConnection().prepareStatement(setSelect);
+            pst.setString(1, setFirstName);
+            pst.setString(2, setLastName);
+            pst.setString(3, setDayOfBirth);
+            pst.setString(4, setDiagnosis);
+            pst.executeUpdate();
         } catch (SQLException | ClassNotFoundException e) {
 
         }
@@ -315,29 +291,38 @@ public class MainWindow extends Configs {
     public void addPatient(ActionEvent actionEvent) {
         MainWindow mainWindow = new MainWindow();
         patientAdd.setOnAction(ActionEvent -> {
-            if(nameText.getText().matches("\\d+")||nameText.getText().equals("")) {
+            if (nameText.getText().matches("\\d+") || nameText.getText().equals("")) {
                 infoBox("Registration not Successfull, \nPlease, enter the Name. \nUse characters only.", "Warning", null);
-            } else if (surnameText.getText().matches("\\d+") ||surnameText.getText().equals("")) {
+            } else if (surnameText.getText().matches("\\d+") || surnameText.getText().equals("")) {
                 infoBox("Registration not Successfull,  \nPlease, enter the Surname \nUse characters only.", "Warning", null);
             } else if (dateOfBirth.getText().matches("[a-zA-Z]+") || dateOfBirth.getText().equals("")) {
                 infoBox("Registration not Successfull,   \nPlease, enter right DOB \nMust contains only numbers \nFormat (dd/mm/yyyy) or (dd.mm.yyyy)", "Warning", null);
             } else if (diagnosisText.getText().equals("")) {
                 infoBox("Registration not Successfull,   \nPlease, enter Diagnosis", "Success", null);
-            }else {
+            } else {
                 mainWindow.addPatients(nameText.getText(), surnameText.getText(), dateOfBirth.getText(), diagnosisText.getText());
                 infoBox("Registration Successfull", "Success", null);
             }
         });
 
     }
-    public static void infoBox(String infoMessage, String titleBar, String headerMessage)
-    {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle(titleBar);
-        alert.setHeaderText(headerMessage);
-        alert.setContentText(infoMessage);
-        alert.showAndWait();
+
+
+    public void getSelected(javafx.scene.input.MouseEvent mouseEvent) {
+
+        index = getPatientTable.getSelectionModel().getSelectedIndex();
+        if (index <= -1) {
+            return;
+        }
+//        idText.setText(patientID.getCellData(index));
+        nameText1.setText(patientName.getCellData(index));
+        surnameText1.setText(patientSurname.getCellData(index));
+        dobText.setText(patientAge.getCellData(index));
+        diagnosisText1.setText(patientDiagnosis.getCellData(index));
     }
+
+
+
 //----------------------------------------------------------------------------------------------------------------------
 
 
@@ -380,75 +365,17 @@ public class MainWindow extends Configs {
 //    }
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-//    public void onEdit(){
-//
-//     if (getPatientTable.getSelectionModel().getSelectedItem() != null) {
-//         Patients selectedPatient = (Patients) getPatientTable.getSelectionModel().getSelectedItem();
-//         patientID.setText(String.valueOf(selectedPatient.getId()));
-//         patientName.setText(selectedPatient.getFirstName1());
-//         patientSurname.setText(selectedPatient.getLastName1());
-//         patientAge.setText(selectedPatient.getDayOfBirth1());
-//
-//
-//     }
-//    }
-
-
-//        Connection connection = getDbConnection();
-//        data = FXCollections.observableArrayList();
-//
-////        ObservableList<String> row = FXCollections.observableArrayList();
-////        try{
-////            PreparedStatement preparedStatement = c.prepareStatement("SELECT * FROM hospital_db.patients  where id =? AND first_name =? AND second_name =? AND day_of_birth =?");
-////            ResultSet rs = preparedStatement.executeQuery();
-////            while(rs.next()) {
-////                row.add(String.valueOf(new Patients(Integer.parseInt(rs.getString("ID")), rs.getString("Name"), rs.getString("Surname"), rs.getString("Age"))));
-////
-////            }
-////        } catch (SQLException e) {
-////            throw new RuntimeException(e);
-////        }
-//
-//        try {
-////            String selectPatients = "SELECT * FROM hospital_db.patients";
-//            String selectPatients = "SELECT id, first_name, second_name, day_of_birth FROM hospital_db.patients";
-//
-////            String selectDiagnosis = "SELECT diagnosis FROM hospital_db.patients";
-////            ResultSet rsDiagnosis = c.createStatement().executeQuery(selectDiagnosis);
-//
-//
-//            ResultSet rs = connection.createStatement().executeQuery(selectPatients);
-//            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
-//                //We are using non property style for making dynamic table
-//                final int j = i;
-//                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
-//                col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
-//
-//                getPatientTable.getColumns().addAll(col);
-//
-//
-//            }
-//            while (rs.next()) {
-//                //Iterate Row
-//                ObservableList<String> row = FXCollections.observableArrayList();
-//                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
-//                    //Iterate Column
-//                    row.add(rs.getString(i));
-//
-//                }
-//                data.add(row);
-//            }
-//            getPatientTable.getItems().clear();
-//
-//            getPatientTable.getItems().addAll(data);
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
+    public static void infoBox(String infoMessage, String titleBar, String headerMessage) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(titleBar);
+        alert.setHeaderText(headerMessage);
+        alert.setContentText(infoMessage);
+        alert.showAndWait();
     }
+
+}
+
+
 
 
 
